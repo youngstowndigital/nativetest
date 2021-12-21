e05655cd4a0839dcf47c9263da7f70de90a96580
Mike Wittenauer <mikewittenauer@me.com>
2021-12-20 21:09:49 -0500

    Initial Commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import Cat from './components/Cat';
+
+const App = () => {
+  return (
+    <ScrollView>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Cat />
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
+  );
+}
+
+export default App;
